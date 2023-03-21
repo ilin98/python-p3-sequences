670ec8b4ee094b4670a65b34f61534a800c0e46a
Isaac <isaac.lin.pnc@gmail.com>
2023-03-21 10:25:31 -0400

    finished lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]
+    i = 2
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print(sequence)
+    else:
+        while i < length:
+            new_num = sequence[len(sequence) - 1] + sequence[len(sequence) - 2]
+            sequence.append(new_num)
+            i += 1
+        print(sequence)
